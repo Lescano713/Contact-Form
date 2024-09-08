f4c7e75b51c4ecb3bbdecbf2181a52407967e9d7
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2024-09-07 23:59:27 -0500

    adding the class to the errors elements

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const inputsName = document.querySelectorAll('input[type="text"]')
+const inputsName = document.querySelectorAll('input')
 const submit = document.querySelector('button[type="submit"]')
 
 function validationName(){
-    console.log('a')
     inputsName.forEach(input => {
-        console.log(input.value)
+        const container = input.closest('label');
         if (input.value === "") {
-            alert('falla')
-            const container = input.closest('label');
             const p = document.createElement('p');
+            p.classList.add('errorState')
             p.textContent = "The field is required"
             container.appendChild(p)
+        }else{
+            alert('good')
         }
     });
 }
