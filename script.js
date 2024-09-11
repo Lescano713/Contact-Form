1addf7515fcce3c492b33df2400c4a7cf0977d36
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2024-09-10 23:10:04 -0500

    clean after submit

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-const inputsName = document.querySelectorAll('input')
-const submit = document.querySelector('button[type="submit"]')
+const inputsName = document.querySelectorAll('input');
+const submit = document.querySelector('button[type="submit"]');
+
+const response = [];
+
+// const validation = {
+//     'email': 
+// }
 
 function validationName(){
     inputsName.forEach(input => {
         const container = input.closest('label');
         if (input.value === "") {
             const p = document.createElement('p');
-            p.classList.add('errorState')
-            p.textContent = "The field is required"
+            p.classList.add('errorState');
+            p.textContent = "The field is required";
             container.appendChild(p)
         }else{
+            input.value = "";
             const successMessage = document.createElement('article');
             successMessage.classList.add('success-message')
             successMessage.innerHTML =`
             <h3 class="success-title">Message Sent!</h3>
             <p>Thanks for completing the form, We'll be in touch soon!</p>`
             document.body.appendChild(successMessage)
+            
         }
     });
 }
@@ -23,3 +31,19 @@ submit.addEventListener('click', e=>{
     e.preventDefault()
     validationName();
 })
+
+// function validation(input,container){
+//     input.value = key;
+//     switch (key) {
+//         case "":
+//             const p = document.createElement('p');
+//             p.classList.add('errorState')
+//             p.textContent = "The field is required"
+//             container.appendChild(p)
+//             break;
+//         case "":
+//             break
+//         default:
+//             break;
+//     }
+// }
